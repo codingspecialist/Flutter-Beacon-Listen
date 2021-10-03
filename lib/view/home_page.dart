9b729c4097f8a882e085d624f892a94d1e1c5a23
이지원 <jwlee@ijiwon-ui-MacBookPro.local>
2021-10-03 18:30:16 +0900

    2.2 Beacon 수신하기

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_beacon/flutter_beacon.dart';
@@ -12,6 +15,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool _isListening = false;
+  final regions = <Region>[];
+  StreamSubscription? beaconStream;
+  List<Beacon> beacons = [];
 
   @override
   void initState() {
@@ -19,10 +25,26 @@ class _HomePageState extends State<HomePage> {
     _initFlutterBeacon();
   }
 
+  @override
+  void dispose() {
+    beaconStream?.cancel();
+    super.dispose();
+  }
+
   void _initFlutterBeacon() async {
     try {
       await flutterBeacon.initializeScanning;
       await flutterBeacon.initializeAndCheckScanning; // 권한체크=>자동권한요청
+
+      if (Platform.isIOS) {
+        regions.add(Region(
+          identifier: 'Test',
+          proximityUUID: '39ED98FF-2900-441A-802F-9C398FC199D2', // 수신할 UUID
+        ));
+      } else {
+        regions.add(Region(identifier: 'Test')); // Android는 모든 Beacon 수신 가능
+      }
+
       print(
           '\x1B[95m========== Flutter Beacon Initialized ==========\x1B[105m');
     } on PlatformException catch (e) {
@@ -43,6 +65,19 @@ class _HomePageState extends State<HomePage> {
           children: [
             ElevatedButton(
               onPressed: () {
+                if (_isListening) {
+                  beaconStream!.pause(); // 리스닝 일시정지
+                  beacons = [];
+                } else {
+                  // 스크림구독이 초기화 됐으면 재시작 하고, 널이면
+                  beaconStream == null
+                      ? beaconStream =
+                          flutterBeacon.ranging(regions).listen((r) {
+                          beacons = r.beacons;
+                          setState(() {});
+                        })
+                      : beaconStream!.resume();
+                }
                 _isListening = !_isListening;
                 setState(() {});
               },
@@ -50,13 +85,14 @@ class _HomePageState extends State<HomePage> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: 10,
+                itemCount: beacons.length,
                 itemBuilder: (context, index) {
+                  Beacon beacon = beacons[index];
                   return BeaconItem(
-                    uuid: "9dba0f8c-5ac5-4cc5-8aa3-032d1d2c1de3",
-                    majorId: 1,
-                    minorId: 100,
-                    accuracy: 0.2,
+                    uuid: beacon.proximityUUID,
+                    majorId: beacon.major,
+                    minorId: beacon.minor,
+                    accuracy: beacon.accuracy,
                   );
                 },
               ),
